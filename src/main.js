0d44899bf265abcbdac5abdbc91deb28cda9e8f1
wang <1348958548@qq.com>
2019-07-08 22:37:26 +0800

    配置REM适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+import './styles/index.less'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
