30e764be511cb09e49499c040dfc0f84e04eb2c8
wang <1348958548@qq.com>
2019-07-14 21:27:54 +0800

    当前频道的文章列表加载渲染， 当没有数据的时候停止加载

--- src/api/article.js
+++ src/api/article.js
@@ -1,8 +1,7 @@
 import request from '@/utils/request'
 
-
 // 获取推荐频道文章列表数据
-export const getArticle = ({
+export const getArticles = ({
   channelId, // 频道ID
   timestamp, // 时间戳 （用于分页）
   withTop // 是否包含置顶，进入页面第一次请求时，要包含置顶的文章，1 表示包含置顶，0 表示不包含置顶
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,21 +3,22 @@
     <van-nav-bar title="首页"/>
     <van-tabs v-model="activeChangeIndex">
       <van-tab
-      :title="item.name"
-      v-for="item in channels"
-      :key="item.id"
+      :title="channelItem.name"
+      v-for="channelItem in channels"
+      :key="channelItem.id"
       >
-        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+        <van-pull-refresh
+        @refresh="onRefresh">
           <van-list
-            v-model="loading"
-            :finished="finished"
+            v-model="channelItem.upPullLoading"
+            :finished="channelItem.upPullFinished"
             finished-text="没有更多了"
             @load="onLoad"
           >
             <van-cell
-              v-for="item in list"
-              :key="item"
-              :title="item"
+              v-for="activeItem in channelItem.articles"
+              :key="activeItem.art_id"
+              :title="activeItem.title"
             />
           </van-list>
         </van-pull-refresh>
@@ -29,6 +30,7 @@
 
 <script>
 import { getUserChannels } from '@/api/channels'
+import { getArticles } from '@/api/article'
 
 export default {
   name: 'home',
@@ -36,33 +38,56 @@ export default {
     return {
       channels: [], // 频道列表
       activeChangeIndex: 0,
-      list: [],
-      loading: false,
-      finished: false,
-      isLoading: false
     }
   },
   created () {
     this.loadChannels()
   },
+  // 计算属性
+  computed: {
+    // 当前激活的频道
+    // const { id: channelId } = this.channels[this.activeChangeIndex]
+    // 当前激活的频道————多个地方使用，封装到计算属性当中
+    activeChannel () {
+      return this.channels[this.activeChangeIndex]
+    }
+  },
   methods: {
-    // 向下滚动刷新
-    onLoad () {
+    // 上拉加载更多， push数据
+    async onLoad () {
       // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
+      let data = []
+      data = await this.loadArticles()
+      // 如果没有 时间戳了，并且文章列表为空， 表示没有数据了
+      if (!data.pre_timestamp && !data.results.length) {
+        // 取消加载loading
+        this.activeChannel.upPullLoading = false
+        // 设置频道已经加载完毕，并且不再onload
+        this.activeChannel.upPullFinished = true
+        return
+      }
+      // console.log(data)
+      // 如果是初次加载频道文章列表，没有数据（没有数据就加载上一次的数据）
+      if (data.pre_timestamp && !data.results.length) {
+        // 当前激活频道的时间戳
+        this.activeChannel.timestamp = data.pre_timestamp
+        data = await this.loadArticles()
+      }
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+      // 数据加载好之后，把时间戳 更新到当前频道的最新 时间戳 来加载下页数据
+      this.activeChannel.timestamp = data.pre_timestamp
+
+      // this.activeChannel.articles = data.results
+      // 不能直接把数据复制，这样就覆盖了原来的数据了
+      
+      // 要使用push 追加 把数组的所有元素push 到文章列表
+      this.activeChannel.articles.push(...data.results)
+      
+      // 数据加载完毕后  取消上了loading
+      this.activeChannel.upPullLoading = false
+      console.log(data)
     },
-    // 下拉加载
+    // 下拉刷新数据
     onRefresh () {
       setTimeout(() => {
         this.$toast('刷新成功')
@@ -95,12 +120,23 @@ export default {
       channels.forEach(item => {
         // 用来遍历当前频道的数据和状态
         item.articles = [] // 存储频道的文章列表
+        item.timestamp = Date.now()
         item.downPullLoading = false // 下拉刷新状态
         item.upPullLoading = false // 上拉加载状态
         item.upPullFinished = false // 是否加载完毕
       })
 
       this.channels = channels
+    },
+    async loadArticles () {
+      const { id:channelId ,timestamp } = this.activeChannel
+      const data = await getArticles({
+        channelId, // 当前激活频道的ID
+        timestamp,
+        withTop: 1
+      })
+      // 返回数据
+      return data
     }
   }
 }
