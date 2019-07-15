a0761fc7d6214383398f6fbca51b9f16e5c310a6
wang <1348958548@qq.com>
2019-07-15 11:04:04 +0800

    路由缓存 keep-view

--- src/views/footer-layout/index.vue
+++ src/views/footer-layout/index.vue
@@ -3,7 +3,7 @@
   <router-view></router-view>
   <van-tabbar>
     <van-tabbar-item icon="wap-home" to="/">首页</van-tabbar-item>
-    <van-tabbar-item icon="smile-comment-o">问答</van-tabbar-item>
+    <van-tabbar-item icon="smile-comment-o" to="/p">问答</van-tabbar-item>
     <van-tabbar-item icon="tv-o">视频</van-tabbar-item>
     <van-tabbar-item icon="contact">我的</van-tabbar-item>
   </van-tabbar>
