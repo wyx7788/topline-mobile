51ef8a0dc2e255dd9e7cd7226a133c8bb2091b45
wang <1348958548@qq.com>
2019-07-22 19:28:39 +0800

    实现token过期，重新通过refresh_token 刷新token ，然后加载请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 import JSONbig from 'json-bigint'
+import router from '@/router'
 // axios.create  用于创建一个  axios 实例， 该实例和 axios 的功能一模一样 。
 // 说白了就是 克隆了一个  axios
 // 为什么要这样做呢， 我们 可以拥有 多个不同的请求函数， 而他们的配置可能是 不一样的
@@ -44,14 +45,51 @@ request.interceptors.request.use(function (config) {
 })
 
 // Add a response interceptor
+// 响应拦截器
 request.interceptors.response.use(function (response) {
   // Do something with response data
 
   // 如果响应结果对象中有 data，则直接返回这个 data 数据
   // 如果响应结果对象中没有 data，则不作任何处理，直接原样返回这个数据
   return response.data.data || response.data
-}, function (error) {
+}, async (error) => {
   // Do something with response error
+  // console.dir(error)
+  // token 过期后判断状态码 401
+  if (error.response.status === 401) {
+    // 判断是否有登录信息
+    if (!store.state.user) {
+      // 没有登录直接去登陆
+      router.push({
+        name: 'login'
+      })
+      return
+    }
+    // 如果是有登录信息了，使用refresh_token  重新获取token
+    try {
+      const { data } = await axios({
+        method: 'PUT',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${store.state.user.refresh_token}`
+        }
+      })
+      // 新的token  获取成功
+      // 通过vue x 容器更改数据
+      store.commit('setUser', {
+        token: data.data.token,
+        refresh_token: store.state.user.refresh_token
+      })
+      // 继续发送刚才的请求
+      // 非刷新的token 的请求，使用request 走正常请求拦截器
+      return request(error.config)
+    } catch (error) {
+      // token 请求失败，就直接跳转登录吧
+      router.push({
+        name: 'login'
+      })
+    }
+  }
   return Promise.reject(error)
 })
 
