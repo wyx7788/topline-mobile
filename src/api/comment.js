7aec4fac7d88736bc23e8f6585c9a6c6163c3fb2
wang <1348958548@qq.com>
2019-07-26 11:44:51 +0800

    添加评论（回复）接口封装

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 评论（回复）列表 接口
 export const getComments = ({
   isArticle = true,
   source,
@@ -14,6 +15,23 @@ export const getComments = ({
       source,
       offset,
       limit
+    }
+  })
+}
+
+// 添加评论（回复） 接口
+export const addComments = ({
+  target, // 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）
+  content, // 评论内容
+  articleId = null, // 对文章进行评论，不要传此参数。
+}) => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/comments',
+    data: {
+      target, // 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）
+      content, // 评论内容
+      art_id: articleId, // 文章id，对评论内容发表回复时，需要传递此参数，表明所属文章id。对文章进行评论，不要传此参数。
     }
   })
 }
